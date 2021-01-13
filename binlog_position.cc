a22abf46db6f9c9971fb0485c3c5e9552346ab0e
Kir Shatrov <shatrov@me.com>
2021-01-13 17:40:01 +0300

    WIP

--- binlog_position.cc
+++ binlog_position.cc
@@ -164,13 +164,14 @@ int BinlogPosition::Update(RawLogEventData event, off_t end_offset) {
       break;
     }
     case constants::ET_TABLE_MAP: {
-      TableMapEvent ev;
-      if (!ev.ParseFromRawLogEventData(event)) {
-        LOG(ERROR) << "Failed to parse QueryEvent";
-        monitoring::rippled_binlog_error->Increment(
-          monitoring::ERROR_PARSE_QUERY);
-        return -1;
-      }
+      parseTableMapEvent(event.event_data, event.event_data_length);
+      // TableMapEvent ev;
+      // if (!ev.ParseFromRawLogEventData(event)) {
+      //   LOG(ERROR) << "Failed to parse QueryEvent";
+      //   monitoring::rippled_binlog_error->Increment(
+      //     monitoring::ERROR_PARSE_QUERY);
+      //   return -1;
+      // }
       group_state = END_OF_GROUP;
       break;
     }
@@ -267,3 +268,44 @@ std::string BinlogPosition::ToString() const {
 }
 
 }  // namespace mysql_ripple
+
+void parseTableMapEvent(const uint8_t *buffer, int len) {
+  // the format descriptor event consists of some fixed size parts
+  // and then a variable section and finally the checksum TRUE/FALSE in the end.
+  // we don't (currently) care about the variable section.
+  // const int fixed_len = 2 + kServerVersionStringLen + 4 + 1;
+  // if (len < fixed_len) {
+  //   return false;
+  // }
+  uint64_t table_id;
+  std::string schema_name;
+  std::string table_name;
+
+
+  table_id = byte_order::load6(buffer);
+
+  uint8_t schema_name_len = byte_order::load1(buffer + 8);
+  schema_name.assign(reinterpret_cast<const char*>(buffer + 8), schema_name_len);
+
+  uint8_t table_name_len = byte_order::load1(buffer + 8 + schema_name_len);
+  table_name.assign(reinterpret_cast<const char*>(buffer + 8 + schema_name_len), table_name_len);
+
+  LOG(INFO) << "schema_name: " << schema_name
+            << " table name: " << table_name
+            << ", table_id: " << std::to_string(table_id);
+
+  // TODO: printf!
+
+  // const char *ptr = reinterpret_cast<const char*>(buffer + 2);
+  // size_t version_len = strnlen(ptr, kServerVersionStringLen);
+  // server_version = std::string(ptr, version_len);
+  // create_timestamp = byte_order::load4(buffer + kServerVersionStringLen + 2);
+  // event_header_length = byte_order::load1(buffer + kServerVersionStringLen +
+  //                                         2 + 4);
+  // for (uint offset = kServerVersionStringLen + 2 + 4 + 1; offset < len - 1;
+  //      offset++) {
+  //   event_type_header_lengths.push_back(byte_order::load1(buffer + offset));
+  // }
+  // checksum = byte_order::load1(buffer + len - 1);
+  // return true;
+}
